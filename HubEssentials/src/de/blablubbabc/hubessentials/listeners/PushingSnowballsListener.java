e9cf55f5917998d6417b7ba488687a8c51bf668c
blablubbabc <lukas@wir-sind-wir.de>
2013-10-14 23:25:55 +0200

    Improved pushing snowballs.

--- HubEssentials/src/de/blablubbabc/hubessentials/listeners/PushingSnowballsListener.java
+++ HubEssentials/src/de/blablubbabc/hubessentials/listeners/PushingSnowballsListener.java
@@ -5,11 +5,12 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.util.Vector;
 
 import de.blablubbabc.hubessentials.HubEssentials;
 
 public class PushingSnowballsListener extends AbstractListener {
-
+	
 	public PushingSnowballsListener(HubEssentials plugin) {
 		super(plugin);
 	}
@@ -21,7 +22,17 @@ public class PushingSnowballsListener extends AbstractListener {
 		if (damager.getType() == EntityType.SNOWBALL) {
 			Player target = (Player) event.getEntity();
 			if (!target.hasPermission(plugin.BYPASS_PUSHING_PERMISSION)) {
-				target.setVelocity(damager.getVelocity().setY(0).normalize().multiply(plugin.config.pushingSnowballsForce).setY(plugin.config.pushingSnowballsForceY));
+				Vector velocity = damager.getVelocity();
+				double length = velocity.length();
+				// y value:
+				velocity.setY(plugin.config.pushingSnowballsForceY).normalize();
+				// final strength:
+				velocity.multiply(length * plugin.config.pushingSnowballsForce);
+				
+				// just in case:
+				if (velocity.lengthSquared() > 10) velocity.normalize().multiply(10);
+				
+				target.setVelocity(velocity);
 			}
 		}
 	}
